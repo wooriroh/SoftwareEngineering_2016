a2dfbb428df003fbb99c2c6839abb250492394d8
Daniel Amkær Sørensen <daniel.amkaer@gmail.com>
2016-11-26 23:45:19 +0900

    Fixed cors headers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            'cors',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
@@ -53,6 +53,5 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \App\Http\Middleware\SendCorsHeaders::class,
     ];
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +12,14 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Request $r)
     {
-        //
+        if ($r->getMethod() == 'OPTIONS') {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: *');
+            header('Access-Control-Allow-Headers: X-XSRF-TOKEN, Content-Type, Authorization, Accept');
+            die;
+        }
     }
 
     /**
